fd9f17c320f3a39cb8a77f2fc7f471d62f5d416b
Colin Diener <colin@colind.me>
2017-02-18 06:51:18 -0500

    Added scenarios; Added more 'front end'

--- interface.cpp
+++ interface.cpp
@@ -4,13 +4,27 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <locale>
 using namespace std;
+
+/* *
+ * TODO: Hook functions to work with the backends.
+ * 
+ * Hook functions will need to pass in entire command and
+ * return the details of the room / items in order to spit
+ * out the text.
+ * */
+
+
 
 bool is_in(const std::vector<std::string> array, const std::string check);
 
 int main() {
 	
-//	vector<string> MOVEMENT[] = {"NORTH","SOUTH","EAST","WEST",
+	bool RUNNING = true;
+	
+	
+	//	vector<string> MOVEMENT[] = {"NORTH","SOUTH","EAST","WEST",
 //					"FORWARD","BACKWARD","LEFT","RIGHT"};
 
 
@@ -22,45 +36,54 @@ int main() {
 	itemcmd.push_back("PLACE");
 	itemcmd.push_back("LIFT");
 	itemcmd.push_back("RAISE");
+	itemcmd.push_back("DROP");
 	wherecmd.push_back("UP");
 	wherecmd.push_back("DOWN");
 	wherecmd.push_back("BACK");
-	movement.push_back("NORTH");
-	movement.push_back("SOUTH");
-	movement.push_back("EAST");
-	movement.push_back("WEST");
+	wherecmd.push_back("LEFT");
+	wherecmd.push_back("RIGHT");
+	wherecmd.push_back("FORWARD");
+	movement.push_back("WALK");
+	movement.push_back("GO");
+	movement.push_back("HEAD");
+	movement.push_back("PROCEED");
 	
-	string cmd;
-	cout << "COMMAND: ";
-	getline(cin,cmd);
 
-	// Why doesn't C++ have an explode or split? Seriously.
-	string temp;
-	vector <string> splitcmd;
-	stringstream s (cmd);
-	while(s>> temp)
-		splitcmd.push_back(temp);
+	while(RUNNING) {
 
+		string cmd;
+		cout << "COMMAND: ";
+		getline(cin,cmd);
 	
-	cout << "The commands entered were: " << splitcmd[0] << "," << splitcmd[1] << "," << splitcmd[2] << endl;
-	if(is_in(movement,splitcmd[0])) {
-		// TODO
-		// This is a movement handler, and should redirect the rest
-		// of the command to another function that will handle if 
-		// player will be able to actually move. For now, debug.
-		cout << "MOVEMENT\n";
+		// Why doesn't C++ have an explode or split? Seriously.
+		string temp;
+		vector <string> splitcmd;
+		stringstream s (cmd);
+		while(s>> temp)
+			splitcmd.push_back(temp);
+		
+		if(is_in(movement,splitcmd[0])) {
+			// TODO
+			// This is a movement handler, and should redirect the rest
+			// of the command to another function that will handle if 
+			// player will be able to actually move. For now, debug.
+			cout << "MOVEMENT\n";
+		}
+		if(is_in(itemcmd,splitcmd[0])) {
+			// TODO
+			// This will be passed on to another function which will handle
+			// all the action past this.
+			cout << "ITEM\n";
+		}
+		// This causes a segmentation fault if there are not three args
+/*		if(is_in(wherecmd,splitcmd[2])) {
+			// TODO
+			// Man, this is gonna be some hard stuff to do..
+		}
+*/		if(splitcmd[0] == "EXIT" || splitcmd[0] == "QUIT") {
+			RUNNING = false;
+		}
 	}
-	if(is_in(itemcmd,splitcmd[0])) {
-		// TODO
-		// This will be passed on to another function which will handle
-		// all the action past this.
-		cout << "ITEM\n";
-	}
-	if(is_in(wherecmd,splitcmd[2])) {
-		// TODO
-		// Man, this is gonna be some hard stuff to do..
-	}
-
 	return 0;
 }
 
